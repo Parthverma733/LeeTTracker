407f7de7a80de6dcbbb3c9727c967c0173953386
parth verma <parthverma733@gmail.com>
2026-08-08 14:06:18 +0530

    cors updated

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://leet-tracker-f3modvm77-parthverma733s-projects.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
